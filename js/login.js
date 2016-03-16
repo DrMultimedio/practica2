4092f1223a08a375c2609b70a4eb923fb0ba3e0b
josekendo <josevicenteantoncoy@hotmail.com>
2016-03-16 15:21:41 +0100

    cambio en la ruta desde htdocs y login
    no tengo claro la ruta porque falla el post

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-varobj; // variable global que guarda el objeto XMLHttpRequest
+var obj; // variable global que guarda el objeto XMLHttpRequest
 
 function entrar(formulario)
 {
@@ -10,18 +10,20 @@ function entrar(formulario)
 
 function crearObjAjax()
 {
-	varxmlhttp;
+	var xmlhttp;
 	if(window.XMLHttpRequest) 
 	{ 
 		// Objeto nativo
 		xmlhttp= new XMLHttpRequest(); // Se obtiene el nuevo objeto
+		console.log("Detectado tipo de protocolo: estandar");
 	} 
-	elseif(window.ActiveXObject) 
+	else if(window.ActiveXObject) 
 	{ 
 		// IE(Windows): objectoActiveX
 		xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+		console.log("Detectado tipo de protocolo: windows");
 	}
-	returnxmlhttp;
+	return xmlhttp;
 }
 
 
@@ -40,5 +42,53 @@ function peticionAJAX_GET(url,u,p)
 		obj.onreadystatechange= procesarCambio; // función callback: procesarCambio
 		obj.open("GET", url, true); // Se crea petición GET a url, asíncrona ("true")
 		obj.send(); // Se envía la petición
+	}
+}
+
+
+function peticionAJAX_POST(url) 
+{
+	obj = crearObjAjax();
+	if (obj) 
+	{ 	
+		// Si se ha creado el objeto, se completa la petición ...
+		// Argumentos:
+		console.log("se empieza a procesar la peticion");
+		if(url == "/practica2/rest/post/login/")
+		{
+			console.log("la peticion es de caracter login");
+			var login = document.getElementById("userlogin").value;
+			var pass = document.getElementById("password").value;
+		}
+		var args = "login=" + login + "&pwd=" + pass;
+		args += "&v=" + (new Date()).getTime(); // Truco: evita utilizar la cache
+		// Se establece la función (callback) a la que llamar cuando cambie el estado:
+		obj.onreadystatechange = procesarCambio(url); // función callback: procesarCambio
+		obj.open("POST", url, true); // Se crea petición POST a url, asíncrona("true")
+		// Es necesario especificar la cabecera "Content-type" para peticiones POST
+		obj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		obj.send(args); // Se envía la petición
+		console.log("se termina de procesar la informacion");
+		return false;
+	}
+}
+
+function procesarCambio(url)
+{
+	if(obj.readyState == 4)
+	{ 
+		// valor 4: respuesta recibida y lista para ser procesada
+		if(obj.status == 200)
+		{ 
+			// El valor 200 significa "OK"
+			// Aquí se procesa lo que se haya devuelto:
+			console.log("se ha terminado la carga de datos");
+			document.getElementById("devuelto").innerHTML = obj.responseText;
+		}
+		else 
+		{
+			console.warn("no se ha podido completar la peticion html");
+			alert("Hubo un problema con los datos devueltos"); // ERROR
+		}
 	}
 }
