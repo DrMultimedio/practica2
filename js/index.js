f00f8f81e36ed95a659e780eeaff2fba514bbac6
josekendo <josevicenteantoncoy@hotmail.com>
2016-04-13 18:05:31 +0200

    estrellitas
    se agregan las estrellitas al index

--- js/index.js
+++ js/index.js
@@ -98,7 +98,22 @@ function foormatear_entradas(a)
 		//fin de la asignacion
 		//la publicamos
 		articulo=document.createElement("article");
-		articulo.innerHTML="<header><h4>"+titulo+"</h4></header><div class = 'hoveroculto'><img src='fotos/"+idp+"/"+foto+"' alt='"+des+"'/><span><p>"+descripcion+"</p><a href='viaje.html?id="+idp+"'><b>Seguir Leyendo</b>&nbsp;-></a></span></div><br/><div class='valoracion'><aside class='estrellas'><span class='icon-star'></span><span class='icon-star'></span><span class='icon-sta'></span><span class='icon-star'></span><span class='icon-star'></span></aside></div><br/><footer><h6>Autor:"+usu+", Fecha de edicion:<time datetime='2016'>"+feinicio+"</time></h6></footer>";
+		valor4="";
+		valor4=valor4+"<header><h4>"+titulo+"</h4></header><div class = 'hoveroculto'><img src='fotos/"+idp+"/"+foto+"' alt='"+des+"'/><span><p>"+descripcion+"</p><a href='viaje.html?id="+idp+"'><b>Seguir Leyendo</b>&nbsp;-></a></span></div><br/>";
+		valor4=valor4+"<div class='valoracion'>";
+		for(h=1;h <= 5;h++)
+		{
+			if(h <= valoracion)
+			{
+				valor4=valor4+"<span class='icon-star' style='color:red;'></span>";	
+			}
+			else
+			{
+				valor4=valor4+"<span class='icon-star'></span>";
+			}
+		}
+		valor4=valor4+"</div><br/><footer><h6>Autor:"+usu+", Fecha de edicion:<time datetime='2016'>"+feinicio+"</time></h6></footer>";
+		articulo.innerHTML=valor4;
 		nodo.appendChild(articulo);
 	}
 }
