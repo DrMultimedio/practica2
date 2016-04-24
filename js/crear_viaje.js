b85b7ba0167a06a0b7181bf8b35e0a739906e805
josekendo <josevicenteantoncoy@hotmail.com>
2016-04-24 23:38:52 +0200

    ya se pueden crear fotos pero sin viajes
    preparate a subirlo a 50

--- js/crear_viaje.js
+++ js/crear_viaje.js
@@ -1,11 +1,14 @@
+var valora=0;
+
 function arranque_per(){
-	if(!sessionStorage.getItem("login_session")){
+	if(!sessionStorage.getItem("login_session"))
+	{
 		document.location.href="login.html";
 	}
 	document.getElementById("crearviaje_cont_img").style.display = "none";//parece una tonteria, pero no se puede comprobar 
 	//estilo css desde javascript, asi que reescribo la propiedad
-
 }
+
 function subirfotopushed(){
 	if(document.getElementById("crearviaje_cont_img").style.display == "none"){
 		document.getElementById("crearviaje_cont_img").style.display = "initial";
@@ -56,6 +59,80 @@ function borrar_foto(j){
 
  	}
 }
-comprueba_tam(){
-	
+
+function envio()
+{
+	peticionAJAX_POST();
+	return false;
+}
+
+function valorac(numero)
+{
+		valor4="";
+		nodo2=document.getElementById("valoraciones");
+		while(nodo2.hasChildNodes())//con esto eliminamos todas las estrellas
+		{
+			nodo2.removeChild(nodo2.firstChild);	
+		}
+		for(h=1;h <= 5;h++)
+		{
+			if(h <= numero)
+			{
+				valor4=valor4+"<span class='icon-star' style='color:red;' onclick='valorac("+h+");'></span>";	
+			}
+			else
+			{
+				valor4=valor4+"<span class='icon-star' onclick='valorac("+h+");'></span>";
+			}
+		}
+		nodo2.innerHTML=valor4;
+		valora=numero;
+}
+
+function peticionAJAX_POST(){//peticion ajax para crear un usuario
+	url = "rest/viaje/";
+	obj3 = crearObjAjax();
+	if(obj3)
+	{
+		var fd = new FormData();//formdata agrupa los datos segun clave/valor y los interpreta en el php como las variables de siempre [clave]
+		var ti = document.getElementById("titulo").value;
+		var fh = document.getElementById("fecha_hasta").value;
+		var fde = document.getElementById("fecha_desde").value;
+		var des = document.getElementById("descripcion").value;
+		//var foto =  document.querySelector('input[type=file]').files[0];
+		if(sessionStorage.getItem("login_session"))
+		{
+				fd.append("clave",JSON.parse(sessionStorage.getItem("login_session")).CLAVE);
+				fd.append("login",JSON.parse(sessionStorage.getItem("login_session")).LOGIN);		
+		}
+		fd.append("nombre",ti);//asi agregamos el valor y el nombre de la variable
+		fd.append("descripcion",des);
+		fd.append("fi",fh);
+		fd.append("ff",fde);
+		fd.append("v",valora);
+		obj3.onreadystatechange = procesarCambio3;
+		obj3.open("POST", url, true);
+		//obj3.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); no nos hace falta con formdata
+		obj3.send(fd);//enviamos el formdata
+	}
+}
+
+function procesarCambio3(){ //finalmente, registramos el usuario
+
+
+	if(obj3.readyState == 4){
+		// valor 4: respuesta recibida y lista para ser procesada
+		if(obj3.status == 200)
+		{
+			ver=JSON.parse(obj3.responseText);
+			alert(obj3.responseText);
+			if(ver.RESULTADO == "ok")
+			{
+				//en este punto subiriamos las fotos
+			}
+			
+		}
+
+	}
+
 }
